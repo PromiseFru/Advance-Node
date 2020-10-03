371c968ae127a45c08932272c05fb9ae498a1ca1
Promise Fru <promisefru1@gmail.com>
2020-10-03 13:38:22 +0100

    Authentication Strategies

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 var session = require('express-session');
 var passport = require('passport');
 var ObjectID = require("mongodb").ObjectID;
+var LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -45,6 +46,18 @@ myDB(async client => {
       done(null, doc);
     });
   });
+
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log('User '+ username +' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
 
 }).catch(err => {
   app.route("/").get((req, res) => {
