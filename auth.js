0c6643bbdf6a21a7625aaf96fe19b264569d7b7a
Promise Fru <promisefru1@gmail.com>
2020-10-07 07:51:02 +0100

    imported dependencies

--- auth.js
+++ auth.js
@@ -1,8 +1,22 @@
 var ObjectID = require("mongodb").ObjectID;
 var LocalStrategy = require('passport-local');
 var passport = require('passport');
+var session = require('express-session');
+var passport = require('passport');
 
 module.exports = function (app, myDataBase) {
+    app.use(session({
+        secret: process.env.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+            secure: false
+        }
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
     passport.serializeUser((user, done) => {
         done(null, user._id);
     });
