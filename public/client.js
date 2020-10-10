4bd3a93bdd3bf1ad2717a189385adf6a51cc123d
Promise Fru <promisefru1@gmail.com>
2020-10-10 15:23:39 +0100

    send and display chat messages

--- public/client.js
+++ public/client.js
@@ -18,4 +18,9 @@ $(document).ready(function () {
             (data.connected ? ' has joined the chat.' : ' has left the chat.');
         $('#messages').append($('<li>').html('<b>' + message + '</b>'));
     });
+
+    socket.on('chat message', (data) => {
+        console.log('socket.on 1');
+        $('#messages').append($('<li>').text(`${data.name}: ${data.message}`));
+    });
 });
--- server.js
+++ server.js
@@ -62,8 +62,10 @@ myDB(async client => {
       currentUsers,
       connected: true
     });
-    console.log('user ' + socket.request.user.name + ' connected');
-
+    socket.on('chat message', (message) => {
+      io.emit('chat message', { name: socket.request.user.name, message });
+    });
+    
     socket.on('disconnect', () => {
       console.log('A user has disconnected');
       --currentUsers;
