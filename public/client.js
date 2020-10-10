dabf394c3b813814dbeec4665213388de2fb7b8e
Promise Fru <promisefru1@gmail.com>
2020-10-10 15:15:13 +0100

    message to send defined

--- public/client.js
+++ public/client.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     // Form submittion with new message in field with id 'm'
     $('form').submit(function () {
         var messageToSend = $('#m').val();
-        //send message to server here?
+        socket.emit('chat message', messageToSend);
         $('#m').val('');
         return false; // prevent form submit from refreshing page
     });
